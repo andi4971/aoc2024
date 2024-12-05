949e35e102ebf7fa41549a623229a48b8a250bbc
andi4971 <andi4971@gmail.com>
2024-12-05 14:45:28 +0900

    d5p2

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -7,13 +7,25 @@ fun main() {
         .map { it.split("|").let { it.first().toInt() to it.last().toInt() } }
     val manuals = lines.drop(rules.size + 1)
         .map { it.split(",").map { it.toInt() } }
-        .filter {
-            for(rule in rules) {
-                if (!checkValid(it, rule)) {
-                    return@filter false
+        .mapNotNull {
+            val curr = it.toMutableList()
+            var changed = false
+            val rulesToCheck = rules.toMutableList()
+            while (rulesToCheck.isNotEmpty()) {
+                val rule = rulesToCheck.removeFirst()
+                if (!checkValid(curr, rule)) {
+                    val leftIndex = curr.indexOf(rule.first)
+                    val rightIndex = curr.indexOf(rule.second)
+                    curr[leftIndex] = rule.second
+                    curr[rightIndex] = rule.first
+                    changed = true
+                    rulesToCheck.clear()
+                    rulesToCheck.addAll(rules)
                 }
             }
-            true
+            if(changed) {
+                curr
+            }else null
         }.sumOf { it.elementAt(it.size/2) }
     println(manuals)
 }
